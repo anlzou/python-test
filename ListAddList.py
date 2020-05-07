f1431c7934d45e5a30c6f52bac7127ac40de6908
anlzou <599502931@qq.com>
2020-05-07 10:26:04 +0800

    init(first code file):ListAddList

---
+++ ListAddList.py
@@ -0,0 +1,11 @@
+#list add list
+
+avengers = ['Iron Man','Captain America','Black Widow','The Hulk','Hawkeye','Thor', 'Falcon']
+gotg = ['Star Lord', 'Rocket', 'Groot', 'Drax', 'Gamora']
+
+for y in gotg:
+    avengers.append(y)
+
+print ("Append Nested List Example: ")
+for x in avengers:
+    print (x)
